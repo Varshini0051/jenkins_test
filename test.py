e8a68f97876e849adccb1963b210b424c995dba1
Varshini Sivakumar <126697027+Varshini0051@users.noreply.github.com>
2024-06-29 17:28:18 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ def main():
     print("Hello, World!")
     now = datetime.datetime.now()
     print("Current date and time:", now)
+    print("Sucessfully executed the test.py file")
 
 if __name__ == "__main__":
     main()
